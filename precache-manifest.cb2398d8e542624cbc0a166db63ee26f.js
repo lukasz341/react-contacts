731bde4b0bac8b843c866be9e05adfa13624349f
lukasz341 <lukasz341@tlen.pl>
2018-12-04 22:22:48 +0100

    app contact

---
+++ precache-manifest.cb2398d8e542624cbc0a166db63ee26f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e55142960ce58815987e",
+    "url": "/static/js/main.e5514296.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "e55142960ce58815987e",
+    "url": "/static/css/main.e4d9fa7d.chunk.css"
+  },
+  {
+    "revision": "d49327db5cbf9788167415ade47fe41f",
+    "url": "/index.html"
+  }
+];
